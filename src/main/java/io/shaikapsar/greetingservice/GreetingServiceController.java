5c58e47e07e1c30b5541c96edd884d30243366ad
Apsar, Shaik <shaik.apsar@hotmail.com>
2022-12-25 16:07:30 +0400

    test

--- src/main/java/io/shaikapsar/greetingservice/GreetingServiceController.java
+++ src/main/java/io/shaikapsar/greetingservice/GreetingServiceController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingServiceController {
 
-
 	@GetMapping("/")
 	public String getting() {
-		return "Hello world";
+		return "Hello world!";
 	}
 }
